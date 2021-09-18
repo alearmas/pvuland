992837bb6c3ffc718c6ab583163160855bc25434
Alejandro <aarmas17@gmail.com>
2021-09-18 14:50:04 -0300

    Agregando funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { PrecioApiComponent } from './components/precio-api/precio-api.component';
+import { PronosticoComponent } from './components/pronostico/pronostico.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { SimulacionComponent } from './components/simulacion/simulacion.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    PrecioApiComponent,
+    PronosticoComponent,
+    HeaderComponent,
+    SimulacionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
